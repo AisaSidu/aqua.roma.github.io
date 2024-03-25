d559323e1557254ef7aea040e5f98dc672d9d4fd
azulvela01 <121826809+azulvela01@users.noreply.github.com>
2024-03-25 07:45:27 -0600

    Update bienvenido.php

--- bienvenido.php
+++ bienvenido.php
@@ -6,7 +6,7 @@
         echo'
             <script>
                 alert("Por favor debes iniciar sesión");
-                window.location = "index.php";
+                window.location = "InicioSe.php";
             </script>
         ';
         session_destroy();
